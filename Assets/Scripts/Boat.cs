2528b0fc1702f0c54afbf05460f767c39ac1f0c7
SnowZucc <louis.alexandre@eleve.isep.fr>
2024-04-16 11:29:39 +0300

    Working homemade boat system :) | Add TMP

--- Assets/Scripts/Boat.cs
+++ Assets/Scripts/Boat.cs
@@ -1,18 +1,82 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class Boat : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public GameObject intText;
+    public float interactionDistance;
+    public GameObject TPPointBoat;
+    public GameObject TPPointExterior;
+    private bool isOnBoat = false;
 
     // Update is called once per frame
     void Update()
     {
-        
+        GameObject player = GameObject.FindGameObjectWithTag("Player");
+        float distance = Vector3.Distance(transform.position, player.transform.position);
+
+        if(distance <= interactionDistance)
+        {
+                //Debug.Log("Distance true");
+
+                if (Input.GetKeyDown(KeyCode.E) && !isOnBoat)
+                {
+                    isOnBoat = true;
+                    Debug.Log("Clicked E");
+                    InputManager inputManager = player.GetComponent<InputManager>();
+                    InputManager inputManagerBoat = GetComponent<InputManager>();
+
+                    player.transform.position = TPPointBoat.transform.position;
+
+                    if (inputManager != null)
+                    {
+                        inputManager.enabled = false;
+                    }
+                    else {
+                        Debug.Log("No Inputmanager found for player");
+                    }
+
+                    if (inputManagerBoat !=null)
+                    {
+                        inputManagerBoat.enabled = true;
+                    }
+                    else {
+                        Debug.Log("No Inputmanager found for boat");
+                    }
+                }
+                else if (Input.GetKeyDown(KeyCode.E) && isOnBoat)
+                {
+                    isOnBoat = false;
+                    Debug.Log("Re-Clicked E");
+                    InputManager inputManager = player.GetComponent<InputManager>();
+                    InputManager inputManagerBoat = GetComponent<InputManager>();
+
+                    player.transform.position = TPPointExterior.transform.position;
+                    
+                    if (inputManager != null)
+                    {
+                        inputManager.enabled = true;
+                    }
+                    else {
+                        Debug.Log("No Inputmanager found for player");
+                    }
+
+                    if (inputManagerBoat !=null)
+                    {
+                        inputManagerBoat.enabled = false;
+                    }
+                    else {
+                        Debug.Log("No Inputmanager found for boat");
+                    }
+                }
+        }
+
+        if (isOnBoat)
+        {
+            player.transform.position = TPPointBoat.transform.position;
+            player.transform.rotation = TPPointBoat.transform.rotation;
+        }
     }
 }
